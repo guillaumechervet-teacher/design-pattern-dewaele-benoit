ec6ab010968fd9fe8205ea55203a2c24d20e8f51
dewaele-benoit <benoit.dewaele@hotmail.com>
2018-06-12 12:26:07 +0200

    IArticleDatabase

---
+++ src/Basket/Basket/OrientedObject/Infrastructure/IArticleDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Infrastructure
+{
+    public interface IArticleDatabase
+    {
+        ArticleDatabaseJson GetArticle(string id);
+    }
+}
